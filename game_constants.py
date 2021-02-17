0afffd386ae40d780a642bc7d00e3cdea46fd765
Luc Bergeron <bergeronl@college-montreal.qc.ca>
2021-02-17 13:24:28 -0500

    Udpated to add obstacle types.

--- game_constants.py
+++ game_constants.py
@@ -1,8 +1,19 @@
 """
 List of game constants used.
 """
+#from enum import Enum
+
 
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 768
 SCREEN_TITLE = "Flappy clone"
 MOVEMENT_SPEED = 10
+
+OBSTACLE_DOWNWARD = 1
+OBSTACLE_UPWARD = 2
+OBSTACLE_BOTH = 3
+
+# class ObstacleType(Enum):
+#     DOWNWARD = 1
+#     UPWARD = 2
+#     BOTH = 3
