a0b674239a415ba191057ced762aa4b3d26861ec
Luc Bergeron <bergeronl@college-montreal.qc.ca>
2021-02-19 13:13:32 -0500

    Added an explosion class.

---
+++ explosion.py
@@ -0,0 +1,18 @@
+import arcade
+
+
+class PlaneExplosion(arcade.Sprite):
+    def __init__(self, texture_list):
+        super().__init__()
+
+        # Start at the first frame
+        self.current_texture = 0
+        self.textures = texture_list
+
+    def update(self):
+        self.current_texture += 1
+        if self.current_texture < len(self.textures):
+            self.set_texture(self.current_texture)
+        else:
+            self.remove_from_sprite_lists()
+
