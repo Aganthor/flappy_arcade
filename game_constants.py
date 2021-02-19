e7218a5163b0557a2b13d6e7bca72cacb1534893
Luc Bergeron <bergeronl@college-montreal.qc.ca>
2021-02-19 13:14:30 -0500

    Removed the enum.

--- game_constants.py
+++ game_constants.py
@@ -1,8 +1,6 @@
 """
 List of game constants used.
 """
-#from enum import Enum
-
 
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 768
@@ -11,9 +9,3 @@ MOVEMENT_SPEED = 10
 
 OBSTACLE_DOWNWARD = 1
 OBSTACLE_UPWARD = 2
-OBSTACLE_BOTH = 3
-
-# class ObstacleType(Enum):
-#     DOWNWARD = 1
-#     UPWARD = 2
-#     BOTH = 3
